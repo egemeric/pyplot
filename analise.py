f7ecb28ae8522df70471b89da9cfea36aadf4eaf
Ege Meriç Erdoğan <egemeric99@gmail.com>
2021-05-18 09:25:19 +0300

    added question statistics

--- analise.py
+++ analise.py
@@ -1,4 +1,5 @@
 import csv
+import statistics
 from matplotlib import pyplot
 
 
@@ -35,13 +36,20 @@ def create_dict(surveydata):
 
 
 class AnalyseSurvey:
+    questions = ("Q1", "Q2", "Q3", "Q4", "Q5", "Q6", "Q7", "Q8", "Q9", "Q10", "Q11")
     data_set_dict = dict()
+    QuestionStats = dict()
+    AgeCt = dict()
 
     def __init__(self):
         self.data = create_dict(read_csv_file())
         self.create_nominaldata_keywords()
         self.show_nominal_datas()
-        print(self.data)
+        self.calculate_all_questions_stats()
+
+        print("A produced Data dict:")
+        print(self.data[0])
+        print("-" * 40)
 
     def create_nominaldata_keywords(self):
         for i in self.data:  #
@@ -58,12 +66,17 @@ class AnalyseSurvey:
             print("\t", i, "types:", self.data_set_dict[i])
         print("-" * 40)
 
-    def calculate_mean(self, qusetion, gender="", schooltype="", educationlevel=""):
-        sum = 0
+    def calculate_statistics(self, qusetion):
+        datas = list()
         for i in self.data:
-            sum += i.get(qusetion.upper())
-
-        return sum / len(self.data)
+            datas.append(i.get(qusetion.upper()))
+        return {"mean": statistics.mean(datas),
+                "mode": statistics.mode(datas),
+                "stddev": statistics.stdev(datas),
+                "variance": statistics.variance(datas),
+                "median": statistics.median(datas),
+                "harmonicmean": statistics.harmonic_mean(datas),
+                "quantiles": statistics.quantiles(datas, n=4, method="inclusive")}
 
     def katilimci_yas_araligi_ve_sayisi(self):
         # dict döndürür {yaş: bu yaştaki katılımcı sayısı}
@@ -77,15 +90,22 @@ class AnalyseSurvey:
             age_ct_dict.update({str(i): 0})
         for i in age_all:
             age_ct_dict[str(i)] += 1
+        self.AgeCt = age_ct_dict
         pyplot.bar(list(age_ct_dict.keys()), list(age_ct_dict.values()))
         pyplot.ylabel("Count")
         pyplot.xlabel("Age")
         pyplot.margins(y=0.1)
         pyplot.show()
-        return age_ct_dict
+
+    def calculate_all_questions_stats(self):
+        for i in self.questions:
+            self.QuestionStats.update({i: self.calculate_statistics(qusetion=i)})
 
 
 if __name__ == "__main__":
     analiseobj = AnalyseSurvey()
-    analiseobj.calculate_mean(qusetion="Q1")
+    for key, value in zip(analiseobj.QuestionStats.keys(), analiseobj.QuestionStats.values()):
+        print(key, ": ", value)
+    print("-"*40)
     analiseobj.katilimci_yas_araligi_ve_sayisi()
+    print(analiseobj.AgeCt)
